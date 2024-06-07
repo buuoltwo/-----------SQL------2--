06183df58405f20c4ca8182fe082fb287cb7ed1b
buuoltwo <bullnick20@gmail.com>
2024-06-08 00:32:34 +0800

    distinct之后，如何查询所有字段

--- Sample/Ch02/2_1/List2_9.sql
+++ Sample/Ch02/2_1/List2_9.sql
@@ -1,2 +1,10 @@
+-- SELECT DISTINCT product_type, regist_date
+--   FROM Product;
+
+SELECT * FROM PRODUCT;
+
 SELECT DISTINCT product_type, regist_date
   FROM Product;
+
+-- distinct之后，如何查询所有字段
+SELECT * FROM PRODUCT WHERE PRODUCT_ID IN (SELECT MAX(PRODUCT_ID) FROM PRODUCT GROUP BY product_type, regist_date);
