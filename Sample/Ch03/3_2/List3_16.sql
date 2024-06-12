29c5184e2f4f18eac275166e75724021f5b5084d
buuoltwo <bullnick20@gmail.com>
2024-06-13 01:28:03 +0800

    GROUP BY 子句

--- Sample/Ch03/3_2/List3_16.sql
+++ Sample/Ch03/3_2/List3_16.sql
@@ -1,3 +1,19 @@
 SELECT product_name, purchase_price, COUNT(*)
   FROM Product
  GROUP BY purchase_price;
+
+-- ERROR:  字段 "product.product_name" 必须出现在 GROUP BY 子句中或者在聚合函数中使用
+-- LINE 1: SELECT product_name, purchase_price, COUNT(*)
+--                ^ 
+
+-- 错误:  字段 "product.product_name" 必须出现在 GROUP BY 子句中或者在聚合函数中使用
+-- SQL state: 42803
+-- Character: 8
+
+
+/* 
+SELECT 子句只能存在：
+- 常数
+- 聚合函数
+- GROUP BY 子句指定的列名。（聚合键） 
+*/
