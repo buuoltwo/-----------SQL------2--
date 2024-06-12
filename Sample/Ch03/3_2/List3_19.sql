d6023e13835423f64246f026e3d41e380588defe
buuoltwo <bullnick20@gmail.com>
2024-06-13 01:36:29 +0800

    只有SELECT子句 HAVING子句 ORDER BY子句可以使用聚合函数

--- Sample/Ch03/3_2/List3_19.sql
+++ Sample/Ch03/3_2/List3_19.sql
@@ -2,3 +2,14 @@ SELECT product_type, COUNT(*)
   FROM Product
  WHERE COUNT(*) = 2
  GROUP BY product_type;
+
+-- ERROR:  聚合函数不允许出现在WHERE中
+-- LINE 3:  WHERE COUNT(*) = 2
+--                ^ 
+
+-- 错误:  聚合函数不允许出现在WHERE中
+-- SQL state: 42803
+-- Character: 53
+
+
+--  只有SELECT子句 HAVING子句 ORDER BY子句可以使用聚合函数
