c0a1fae2ded9376433bfb9445b2908a3d3533ae8
buuoltwo <bullnick20@gmail.com>
2024-06-27 00:03:10 +0800

    insert into 通过隐式方法设定默认值

--- Sample/Ch04/4_1/List4_6.sql
+++ Sample/Ch04/4_1/List4_6.sql
@@ -1,3 +1,5 @@
+-- 通过显式方法设定默认值
+
 INSERT INTO ProductIns (product_id, product_name, product_type, sale_price, purchase_price, regist_date) VALUES ('0007', '擦菜板', '厨房用具', DEFAULT, 790, '2009-04-28');
 
 -- 确认插入行
--- Sample/Ch04/4_1/List4_7.sql
+++ Sample/Ch04/4_1/List4_7.sql
@@ -1,1 +1,3 @@
+-- 通过隐式方法设定默认值
+
 INSERT INTO ProductIns (product_id, product_name, product_type, purchase_price, regist_date) VALUES ('0007', '擦菜板', '厨房用具', 790, '2009-04-28');
