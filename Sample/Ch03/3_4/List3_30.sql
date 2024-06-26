ddd13d9bce8edf2683e4c7bfee92ff99f18b6a0c
buuoltwo <bullnick20@gmail.com>
2024-06-27 00:02:53 +0800

    指定多个排序键，对该顺序（或同一顺序）的商品进行更细致的排序

--- Sample/Ch03/3_4/List3_30.sql
+++ Sample/Ch03/3_4/List3_30.sql
@@ -1,3 +1,5 @@
 SELECT product_id, product_name, sale_price, purchase_price
   FROM Product
-ORDER BY sale_price, product_id;
+ORDER BY sale_price, product_id; --sale_price相同时按照product_id的升序排列
+
+-- 指定多个排序键，对该顺序（或同一顺序）的商品进行更细致的排序
